0750accae173dd98bb339b78e26a11dcfba3e5ee
Emre Guneyler <eguneys@gmail.com>
2023-10-17 01:09:30 +0300

    compiled

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,5 +1,7 @@
-scp target/release/duck markahost:~/duck_new
+scp target/release/duck markahost:~/duck/duck_tmp
+scp -r public markahost:~/duck/
+scp -r templates markahost:~/duck/
 ssh markahost systemctl stop duck
-ssh markahost mv duck_new duck
+ssh markahost mv duck/duck_tmp duck/duck
 ssh markahost systemctl start duck
 echo "done."
